bb14b0230ee1499bb77af3231888a0fd576ab372
Josh Bielick <josh@fragmentlabs.com>
2014-02-21 09:19:28 -0500

    grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		uglify: {
+			options: {
+				mangle: false
+			},
+			main: {
+				files: {
+					'src/ligaments.min.js': 'src/ligaments.js'
+				}
+			}
+		},
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					flatten: true,
+					src: ['src/*.js'],
+					dest: 'dist/'
+				}]
+			}
+		},
+		qunit: {
+			files: ['TEST/index.html']
+		},
+		watch: {
+			files: ['src/**/*.coffee'],
+			tasks: ['build']
+		},
+		coffee: {
+			compile: {
+				files: {
+					'src/ligaments.js': 'src/ligaments.coffee'
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-coffee');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.registerTask('test', ['qunit']);
+
+	grunt.registerTask('build', ['coffee:compile', 'uglify:main', 'copy:dist', 'qunit']);
+
+}
